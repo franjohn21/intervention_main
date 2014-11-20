2799356d79414faa354622c708d8801a2313b499
Francis John <francis.john@colorado.edu>
2014-11-20 14:41:12 -0800

    Make johns quick changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def index
-  	puts "hello"
+  end
+
+  def share
+  	puts "hello?"
   end
 end
